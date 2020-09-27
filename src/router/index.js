b6ffc6dd7f4ab0c06136e902de215d715dfbdf4c
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-09-27 07:40:31 +0700

    integrasi

--- src/router/index.js
+++ src/router/index.js
@@ -5,35 +5,49 @@ import Login from '../views/auth/login.vue'
 import Register from '../views/auth/Register.vue'
 import CreatePin from '../views/auth/CreatePin.vue'
 import ResetPassword from '../views/auth/ResetPassword.vue'
+import ChangePassword from '../views/auth/ChangePassword.vue'
 import Main from '../views/Main.vue'
+
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: { requiresVisitor: true }
   },
   {
     path: '/SignUp',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: { requiresVisitor: true }
   },
   {
     path: '/createpin',
     name: 'CreatePin',
-    component: CreatePin
+    component: CreatePin,
+    meta: { requiresVisitor: true }
   },
   {
     path: '/resetpassword',
     name: 'ResetPassword',
-    component: ResetPassword
+    component: ResetPassword,
+    meta: { requiresVisitor: true }
   },
   {
-    path: '/main',
+    path: '/changepassword',
+    name: 'ChangePassword',
+    component: ChangePassword,
+    meta: { requiresVisitor: true }
+  },
+  {
+    path: '/',
     name: 'main',
-    component: Main
+    component: Main,
+    meta: { requiresAuth: true }
   }
 ]
 
@@ -41,6 +55,28 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.isLogin) {
+      next({
+        path: '/login'
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.getters.isLogin) {
+      next({
+        path: '/'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/views/auth/ChangePassword.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-row>
+<!-- bagian kiri halaman untuk banner yg warna biru -->
+      <b-col cols="6" class="backgroud">
+            <SideBarAuth /> <!-- sidebarAuth ada di folder src/components/SideBarAuth.vue -->
+      </b-col>
+<!-- bagian sebelah kanan untuk form isian -->
+      <b-col cols="6" class="bg-white">
+          <b-container class="mt-5">
+                <h2 class="font-h2">
+                    Did You Forgot Your Password?<br>
+                    Don’t Worry, You Can Reset Your<br>
+                    Password In a Minutes.
+                </h2>
+                <p class="font-p">
+                    To reset your password, you must type your e-mail and we<br>
+                    will send a link to your email and you will be directed to the<br>
+                    reset password screens.
+                </p>
+                <FormChangePass /> <!--Bagian form Change Password mengandung komponent dari form password sampai buttom-->
+            </b-container>
+      </b-col>
+  </b-row>
+</template>
+
+<script>
+import SideBarAuth from '../../components/SideBarAuth'
+import FormChangePass from '../../components/FormChangePass'
+export default {
+  name: 'ResetPassword',
+  components: {
+    SideBarAuth,
+    FormChangePass
+  }
+}
+</script>
+
+<style scoped>
+.backgroud {
+    background-color: #6379F4;
+}
+.font-h2 {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 33px;
+    color: #3A3D42;
+}
+.font-p {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 30px;
+    color: rgba(58, 61, 66, 0.6);
+}
+</style>
